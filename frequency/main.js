776c5583b4d2d6ee132c0b7d8690fba7d793ae91
Kify <19012564@es.uveg.edu.mx>
2021-07-29 19:12:42 -0500

    corrección de carpeta XD

---
+++ frequency/main.js
@@ -0,0 +1,7 @@
+// Frequency:
+// Escribir una función frequency que recibe un string como argumento.
+// Esta función debe contar la frecuencia o el número de veces que se repite cada carácter.
+
+// El resultado debe mostrarse en un objeto donde las propiedades sean los caracteres, 
+// y los valores sean la frecuencia. Los resultados deben ordenarse de manera ascendente 
+// por los caracteres y no la frecuencia.
